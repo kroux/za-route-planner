84c02f06d8d10599b5d60c2ab6a15f59f155854f
Jacobus Roux <kobus47@yahoo.com>
2023-04-26 20:58:49 +0200

    Override default Node equality behavior to prevent duplicate nodes in the graph

--- ZARoutePlanner.Core/Node.cs
+++ ZARoutePlanner.Core/Node.cs
@@ -2,5 +2,15 @@ namespace ZARoutePlanner.Core;
 
 public readonly record struct Node(string Name)
 {
+    public bool Equals(Node other)
+    {
+        return Name == other.Name;
+    }
+
+    public override int GetHashCode()
+    {
+        return Name.GetHashCode();
+    }
+
     public HashSet<Edge> Edges { get; } = new();
 }
